b1b6371f6408abef1ec0ac7eea5bdf6afcfc03b6
ahimsaj <90971644+ahimsaj@users.noreply.github.com>
2021-09-22 01:32:59 +0530

    Add files via upload

---
+++ Input Output.py
@@ -0,0 +1,26 @@
+#Input command(Name as input)
+
+Name = input("Enter name: ")
+print("Name :- " + Name)
+
+#Input command(Input a number)
+
+num = int(input("Enter a value: "))
+print("Number + 10 is :- ", num+10)
+
+#Output command
+
+print("y is ,y")
+
+#Format command
+
+print("{} are the good option for beginners in python".format("Books"))
+
+#use of multiple format commands
+
+my_string = "{} are the good option for beginners in {}"
+print(my_string.format("Books","C++"))
+
+#split commands
+
+print("Split name with space ",Name.split())
