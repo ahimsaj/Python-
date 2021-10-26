03370612e9fae02c51e11b9a634b8accabbd0bf2
ahimsaj <90971644+ahimsaj@users.noreply.github.com>
2021-10-26 13:22:30 +0530

    Add files via upload

---
+++ Programs/9th.py
@@ -0,0 +1,13 @@
+#Write a Python program to calculate the sum of three given numbers, 
+# if the values are not - equal then return four times of their sum
+
+a=10
+b=20
+c=30
+s=a+b+c
+if a==b==c:
+    print(s)
+else:
+    print(4*s)
+    
+
