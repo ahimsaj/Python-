58957670784749e583552dd1b57830d8d4e0cddc
ahimsaj <90971644+ahimsaj@users.noreply.github.com>
2021-11-06 00:14:59 +0530

    Add files via upload

---
+++ Programs/26.py
@@ -0,0 +1,25 @@
+#Program to find whether a given number is a strong number or not.
+
+n = int(input("Enter any Number: "))
+s = 0
+t = n
+
+while(t > 0):
+    Factorial = 1
+    i = 1
+    Reminder = t % 10
+
+    while(i <= Reminder):
+        Factorial = Factorial * i
+        i = i + 1
+
+    print("\nFactorial of %d = %d" %(Reminder, Factorial))
+    s = s + Factorial
+    t = t // 10
+
+print("\nSum of Factorials of a Given Number %d = %d" %(n, s))
+
+if (s == n):
+    print("%d is a Strong Number" %n)
+else:
+    print("%d is not a Strong Number" %n)
