536e918b05613c255d4407553bb86ffc6bc440c6
ahimsaj <90971644+ahimsaj@users.noreply.github.com>
2021-11-05 22:13:25 +0530

    Add files via upload

---
+++ Programs/25.py
@@ -0,0 +1,11 @@
+#Program to find factorial of a given number.
+n = int(input("Enter a Number:"))
+factorial=1
+i =1
+while  i<=n:
+    factorial = factorial*i
+    i += 1
+    
+    
+print(factorial)
+    
