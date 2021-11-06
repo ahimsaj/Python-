420b4573913d4e27fa0735701c5edcdc55bb6c0b
ahimsaj <90971644+ahimsaj@users.noreply.github.com>
2021-11-06 13:44:10 +0530

    Add files via upload

---
+++ Programs/36.py
@@ -0,0 +1,10 @@
+#Write a Python program to add 'polis' at the end of a given string (length should be at least 3).
+
+
+str= input("enter string1:")
+print(str)
+
+if("polis" in str):
+        print(str+"CS")
+else:
+    print(str+"polisCS")
